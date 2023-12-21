a9a4ca89107919b7616ac25291b4afce9f8e536d
Taihang Hu <77798564+hutaiHang@users.noreply.github.com>
2023-12-21 15:27:11 +0800

    Update sd_demo.py

--- sd_demo.py
+++ sd_demo.py
@@ -4,23 +4,26 @@ from utils_sd import register_normal_pipeline, register_faster_forward, register
 import time
 
 seed_everything(2023)
-model_id = "runwayml/stable-diffusion-v1-5"
+model_id = "/home/u2120230624/share/runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
+prompt = "a cat wearing sunglasses"
+generator = torch.Generator().manual_seed(2023)
 #warmup gpu for testing time
 print('Warm up of the gpu')
-image = pipe("")
+for i in range(2):
+    image = pipe(prompt)
 #-------------------
 
-prompt = "a cat wearing sunglasses"
+print('Start Generating')
 
 #origin pipeline
 start_time = time.time()
-image = pipe(prompt).images[0] 
+image = pipe(prompt, generator = generator).images[0] 
 end_time = time.time()
 print("Origin Pipeline: {:.3f} seconds".format(end_time-start_time))
-
+image.save("images/orgin_cat.png")
 
 #------------------------------
 register_parallel_pipeline(pipe) # 2. enable parallel. If memory is limited, replace it with  `register_normal_pipeline(pipe)`
@@ -29,9 +32,11 @@ register_faster_forward(pipe.unet)  # 3. encoder propagation
 
 
 #FasterDiffusion pipeline
+generator = torch.Generator().manual_seed(2023)
 start_time = time.time()
-image = pipe.call(prompt).images[0] 
+image = pipe.call(prompt, generator = generator).images[0] 
 end_time = time.time()
 print("Faster Diffusion: {:.3f} seconds".format(end_time-start_time))
+
   
-image.save("cat.png")
+image.save("images/faster_cat.png")
